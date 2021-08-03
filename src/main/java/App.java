cd603bcc3ce422f9543891862b82f4cdfb1b7e0e
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-03 09:51:46 +0300

    added code to get input from the user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your secret string:");
+        String stringSecretWord = scanner.nextLine();
 
     }
 }
