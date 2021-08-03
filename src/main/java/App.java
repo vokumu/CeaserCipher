31221f0d326757fe5de823a8ff3e5fd424407b4a
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-03 10:08:17 +0300

    Added the user choice

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,14 +2,32 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        //CeaserCipher cipher = new CeaserCipher(stringSecretWord, intKey);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your secret string:");
-        String stringSecretWord = scanner.nextLine();
-        System.out.println("Enter your cipher Key:");
-        int intKey = scanner.nextInt();
+        System.out.println("Welcome to Cease Cipher application \nThe application will allow you to encrypt or decrypt your secret message.\nTo do so type your action (Encrypt or Decrypt)");
+        String userTask = scanner.nextLine();
+        String stringSecretWord;
+        int intKey;
+        if(userTask.equals("encrypt")){
+            System.out.println("Enter the sentence you want to encrypt:");
+            stringSecretWord = scanner.nextLine();
+            System.out.println("Enter your cipher Key:");
+            intKey = scanner.nextInt();
+        }
+        else if(userTask.equals("decrypt")){
+            System.out.println("Enter the sentence you want to encrypt:");
+            stringSecretWord = scanner.nextLine();
+            System.out.println("Enter your cipher Key:");
+            intKey = scanner.nextInt();
+
+        }
+        else{
+            System.out.println("Enter the sentence you want to encrypt:");
+        }
+
 
         //Cipher
-        CeaserCipher cipher = new CeaserCipher(stringSecretWord, intKey);
-        System.out.println(cipher.CipherSecretWord(stringSecretWord,intKey));
+        //CeaserCipher cipher = new CeaserCipher(stringSecretWord, intKey);
+        //System.out.println(cipher.CipherSecretWord(stringSecretWord,intKey));
     }
 }
