5808e837da41d65c9c57c298f58f3912b4572aea
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-03 09:16:21 +0300

    initializing the CeaserCipher constructor

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -1,5 +1,9 @@
 public class CeaserCipher {
-    public CeaserCipher(){
-
+    private String secretWord;
+    private int key;
+    //Constructor
+    public CeaserCipher(String secretWord, int key) {
+        this.secretWord=secretWord;
+        this.key=key;
     }
 }
--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -2,4 +2,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CeaserCipherTest {
 
+
 }
