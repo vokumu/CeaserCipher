57b10a14e5f3c4ae527d5dc0ea0ab33bba70971a
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-03 09:37:52 +0300

    added test to the Cipher method2

--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -27,5 +27,23 @@ class CeaserCipherTest {
         String expected = "a";
         assertEquals(expected, testCipher.CipherSecretWord("a", 0));
     }
+    @Test
+    public void caesar_Cypher_shiftCharacterOneStepForward_emptyString_int(){
+        CeaserCipher testCipher=new CeaserCipher("word",2);
+        String expected = "Please type your word";
+        assertEquals(expected, testCipher.CipherSecretWord("", 7));
+    }
+    @Test
+    public void caesar_Cypher_shiftCharacterOneStepForward_string_int_shiftValueGreaterThan25OrLessThan1(){
+        CeaserCipher testCipher=new CeaserCipher("word",2);
+        String expected = "Shift value should be a number greater or equal to 1 and less or equal to 25";
+        assertEquals(expected, testCipher.CipherSecretWord("bhbj", -6));
+    }
+    @Test
+    public void caesar_Cypher_shiftCharacterOneStepForward_string_int_shiftedString(){
+        CeaserCipher testCipher=new CeaserCipher("word",2);
+        String expected = "wxy";
+        assertEquals(expected, testCipher.CipherSecretWord("vwx", 1));
+    }
 
 }
