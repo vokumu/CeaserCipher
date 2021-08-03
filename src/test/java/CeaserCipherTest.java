c924042d2dba917b9d436be170758d2902311505
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-03 09:19:27 +0300

    added testing for instance initialization

--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -1,6 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CeaserCipherTest {
-
-
+    @Test
+    public void newCipher_instantiatesCorrectly() {
+        CeaserCipher testCipher=new CeaserCipher("word",2);
+        assertEquals(true,testCipher instanceof CeaserCipher);
+    }
 }
